86c93a890b9eb2f4bf4831d419e6751f6702b494
hyunsun <sh717510@gmail.com>
2021-06-09 13:18:38 +0900

    jpa repository test

---
+++ src/main/java/com/hyun/springboot/domain/posts/Posts.java
@@ -0,0 +1,38 @@
+package com.hyun.springboot.domain.posts;
+
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length=5000,nullable=false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/com/hyun/springboot/web/PostsApiController.java
@@ -0,0 +1,32 @@
+package com.hyun.springboot.web;
+
+import com.hyun.springboot.service.posts.PostsService;
+import com.hyun.springboot.web.dto.PostsResponseDto;
+import com.hyun.springboot.web.dto.PostsSaveRequestDto;
+import com.hyun.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long Save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,@RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+}
